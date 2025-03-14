de719601881f379676a969f1aa74a32d7e1e9993
HongSungHoon <az0319h@naver.com>
2025-03-14 14:30:07 +0900

    'panda-market(react)'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Root from './pages/Root';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home/Home';
+import FreeBoard from './pages/FreeBoard';
+import SecondHandMarket from './pages/SecondHandMarket';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import FAQ from './pages/FAQ';
+import ProductDetail from './pages/ProductDetail';
 
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Root />,
+    errorElement : <NotFound />,
+    children : [
+      {index : true, element : <Home />},
+      {path : '/product/:detail', element : <ProductDetail />},
+      {path : '/freeboard', element : <FreeBoard />},
+      {path : '/secondhandmarket', element : <SecondHandMarket />},
+      {path : '/privacy', element : <PrivacyPolicy />},
+      {path : '/faq', element : <FAQ />},
+    ]
+  }
+]);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
   );
 }
 
